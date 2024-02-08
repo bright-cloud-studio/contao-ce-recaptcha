51537f2ad472e2622869ae421e0203fbedde594d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-08 15:24:32 -0500

    Create BcsReCaptchaBundle.php

---
+++ src/BcsReCaptchaBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-recaptcha
+ */
+
+namespace Bcs\ReCaptchaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsReCaptchaBundle extends Bundle
+{
+}
