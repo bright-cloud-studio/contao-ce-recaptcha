10f6f0aafcf80c2edb000614456d3cc5979bd7d0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-09 13:09:49 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,14 +3,14 @@
 /**
  * @copyright  Bright Cliud Studio
  * @author     Bright Cloud Studio
- * @package    Contao CE reCAPTCHA
+ * @package    Contao CE Recaptcha
  * @license    LGPL-3.0+
  * @see	       https://github.com/bright-cloud-studio/contao-ce-recaptcha
  */
   
    array_insert($GLOBALS['TL_CTE']['recaptcha'], -1, array
     (
-        'recaptcha_v2' => 'Bcs\GlideBundle\ContentReCaptchaV2',
+        'recaptcha_v2' => 'Bcs\RecaptchaBundle\ContentRecaptchaV2'
     ));
 
 ?>
