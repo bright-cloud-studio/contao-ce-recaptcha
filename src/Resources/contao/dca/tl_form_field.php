b29746ad609f4cda32d1b6674b9dda7310090b78
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-09 14:49:30 -0500

    Create tl_form_field.php

---
+++ src/Resources/contao/dca/tl_form_field.php
@@ -0,0 +1,6 @@
+<?php
+
+
+
+
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['recaptcha'] = '{type_legend},type;{text_legend},text;{expert_legend:hide},class;{template_legend:hide},customTpl;{invisible_legend:hide},invisible';
