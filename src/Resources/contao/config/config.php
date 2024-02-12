930e69327980126fe2efd0177a5fec3a0919ee50
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-12 12:22:39 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -12,6 +12,4 @@ use Bcs\RecaptchaBundle\FormRecaptcha;
 
 array_insert($GLOBALS['TL_FFL']['recaptcha'], -1, FormRecaptcha::class);
 
-/* Hooks */
-$GLOBALS['TL_HOOKS']['compileFormFields'][]      = array('Bcs\Hooks', 'onCompileFormFields');
 ?>
