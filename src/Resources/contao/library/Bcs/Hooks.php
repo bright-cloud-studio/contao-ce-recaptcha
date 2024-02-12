53643d6d37cc5ba3fdd84be8db3861b3a74eeaf4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-12 09:48:19 -0500

    Create Hooks.php

---
+++ src/Resources/contao/library/Bcs/Hooks.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bcs;
+
+use Contao\Database;
+
+class Hooks
+{
+
+    public function onProcessForm(fields, formId, $form)
+    {
+      echo "BING BONG NOISE";
+      die();
+    }
+}
