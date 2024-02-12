5c5cf89388d49464935c128ac9e9c562bb37c70d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-12 10:37:36 -0500

    Update Hooks.php

--- src/Resources/contao/library/Bcs/Hooks.php
+++ src/Resources/contao/library/Bcs/Hooks.php
@@ -14,16 +14,69 @@ class Hooks
         
         // Loop through the form fields, see if this form has our reCAPTCHA Form ELement
         foreach($fields as $index => $field) {
+            // if this field has the 'recaptcha' type then set our flag to true
             if($field->type == 'recaptcha') {
                 $recaptcha = true;
             }
         }
         
+        // if we found a 'recaptcha' type element
         if($recaptcha) {
-            echo "This form is using reCAPTCHA";
-            die();
+            
+            // Loop through the fields again so we can find the submit button
+            foreach($fields as $index => $field) {
+                if($field->type == 'submit') {
+                    //$field['data-callback'] = 'onSubmit';
+                    $field->html = "<div>BLEEP</div>";
+                    //print_r($field);
+                    //die();
+                }
+            }
+            
         }
 
         return $fields;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        public function onPrepareFormData($submittedData, $labels, $fields, $form)
+    {
+
+        $recaptcha = false;
+        
+        // Loop through the form fields, see if this form has our reCAPTCHA Form ELement
+        foreach($fields as $index => $field) {
+            // if this field has the 'recaptcha' type then set our flag to true
+            if($field->type == 'recaptcha') {
+                $recaptcha = true;
+            }
+        }
+        
+        // if we found a 'recaptcha' type element
+        if($recaptcha) {
+            
+            // Loop through the fields again so we can find the submit button
+            foreach($fields as $index => $field) {
+                if($field->type == 'submit') {
+                    //$field['data-callback'] = 'onSubmit';
+                    $field->html = "<div>BLEEP</div>";
+                    //print_r($field);
+                    //die();
+                }
+            }
+            
+        }
+
+        return $fields;
+    }
+    
+    
 }
