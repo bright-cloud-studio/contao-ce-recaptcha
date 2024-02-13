16cab4d63cb14775e8a9e047313e9fbe36143dcf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-13 11:53:05 -0500

    Create default.php

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TL_LANG']['ERR']['recaptcha'] = "reCAPTCHA was not able to verify your authenticity. Please try again or contact us for support.";
+$GLOBALS['TL_LANG']['ERR']['recaptcha_rgxp'] = "reCAPTCHA actions can only include the letters a-z (lower and upper case) and the symbols _ and /.";
+
+$GLOBALS['TL_LANG']['CTE']['miscellaneous'] = "Miscellaneous";
+$GLOBALS['TL_LANG']['CTE']['backgroundrecaptcha'][0] = "Background reCAPTCHA v3";
