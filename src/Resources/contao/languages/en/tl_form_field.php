d64b09a13ef30b877537d595c1cec8fc722df36c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-13 14:21:32 -0500

    Update tl_form_field.php

--- src/Resources/contao/languages/en/tl_form_field.php
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$GLOBALS['TL_LANG']['FFL']['recaptha'] = "reCaptcha field";
+$GLOBALS['TL_LANG']['FFL']['recaptha'][0] = "reCaptcha field";
 $GLOBALS['TL_LANG']['tl_form_field']['recaptcha3_threshold'][0] = "Score threshold";
 $GLOBALS['TL_LANG']['tl_form_field']['recaptcha3_threshold'][1] = "reCAPTCHA v3 returns a score, based on which you can decide if a user is likely a bot or a human. A score of 1 most likely resembles a human, a score of 0 is most likely a bot. Any captcha request made has to be above this score to be considered safe.";
 
