17ae30e780c320c205959ec5495078343ac87131
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-27 14:02:16 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -9,12 +9,13 @@
 */
 
 use Bcs\RecaptchaBundle\FormRecaptcha;
-use Bcs\RecaptchaBundle\ElementRecaptcha;
 
 // Setup our Form field
 array_insert($GLOBALS['TL_FFL']['recaptcha'], -1, FormRecaptcha::class);
 
 // Setup our background content element
-array_insert($GLOBALS['TL_CTE']['miscellaneous'], 0, ElementRecaptcha::class);
+array_insert($GLOBALS['TL_CTE']['miscellaneous'], 0, [
+    'backgroundrecaptcha' => Bcs\ElementRecaptcha::class,
+]);
 
 ?>
