e60ebf817bf36d371a91c513e8c1b98151cbb038
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-28 08:50:02 -0400

    Update Handler.php

--- src/Resources/contao/library/Bcs/Handler.php
+++ src/Resources/contao/library/Bcs/Handler.php
@@ -2,22 +2,19 @@
 
 namespace Bcs;
 
-use Contao\Database;
-
 class Handler
 {
     protected static $arrUserOptions = array();
-
+    
     public function onCompileFormFields($submittedData, $formData, $files, $labels, $form)
     {
-
-      echo "Hook 'onCompileFormFields' triggered!";
-      die();
-
+        
+        echo "Hook 'onCompileFormFields' triggered!";
+        die();
         
         if($formData['formID'] == 'directory_submission') {
-          echo "Form Found!";
-          die();
+            echo "Form Found!";
+            die();
         }
     }
 }
